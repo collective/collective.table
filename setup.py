568f728ea3108e444a9068ace921194352b3f5d2
Martijn Pieters <mj@jarn.com>
2011-01-11 13:39:28 +0000

    Start a new package with some boilerplate
    git-svn-id: http://svn.plone.org/svn/collective/collective.table/trunk@230189 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.table',
+      version=version,
+      description="Table-like content for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        "Framework :: Plone",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        ],
+      keywords='plone table data',
+      author='Jarn AS',
+      author_email='info@jarn.com',
+      url='http://svn.plone.org/svn/collective/collective.table',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Products.CMFPlone',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
