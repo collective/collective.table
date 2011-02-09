fe8b86e6b1741123fd608f8e527afa31a40935ba
Martijn Pieters <mj@jarn.com>
2011-02-09 17:55:47 +0000

    We are our own storage, block all attempts to set it or query info.
    git-svn-id: http://svn.plone.org/svn/collective/collective.table/trunk@231837 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/table/field_widget.py
+++ collective/table/field_widget.py
@@ -1,6 +1,7 @@
 # To manage expectations, the data table is managed as an archetypes field
 from zope import component, interface, schema
 from AccessControl import ClassSecurityInfo
+from Products.Archetypes.exceptions import ObjectFieldException
 from Products.Archetypes.Field import ObjectField
 from Products.Archetypes.Widget import TypesWidget
 from Products.Archetypes.Registry import registerField
@@ -28,6 +29,28 @@ class DataTableField(ObjectField):
         source = u'local'
         return component.getMultiAdapter(
             (self, instance), ISource, name=source)
+
+    # We are not a field in the usual manner, so we need to override some
+    # things to clean things up
+    security.declarePrivate('setStorage')
+    def setStorage(self, instance, storage):
+          raise ObjectFieldException, "Storage setting not supported"
+
+    security.declarePrivate('getStorage')
+    def getStorage(self, instance=None):
+        return self
+
+    security.declarePublic('getStorageName')
+    def getStorageName(self, instance=None):
+        """Return the storage name that is configured for this field
+        as a string"""
+        return ""
+
+    security.declarePublic('getStorageType')
+    def getStorageType(self, instance=None):
+        """Return the type of the storage of this field as a string"""
+        return ""
+    
 
 registerField(DataTableField, 
     title='Data Table', description='Complex data table with flexible sources')
