0fc2bd25d8279c27f30d29cbaf066ba8948f184a
Martijn Pieters <mj@jarn.com>
2011-02-09 15:00:38 +0000

    Proper security so various widget macros don't blow up.
    git-svn-id: http://svn.plone.org/svn/collective/collective.table/trunk@231827 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/table/field_widget.py
+++ collective/table/field_widget.py
@@ -1,7 +1,9 @@
 # To manage expectations, the data table is managed as an archetypes field
 from zope import component, interface, schema
+from AccessControl import ClassSecurityInfo
 from Products.Archetypes.Field import ObjectField
 from Products.Archetypes.Widget import TypesWidget
+from Products.Archetypes.Registry import registerField
 
 from .interfaces import ISource, ITableField
 
@@ -18,12 +20,17 @@ class DataTableWidget(TypesWidget):
 class DataTableField(ObjectField):
     interface.implements(ITableField)
     widget = DataTableWidget
+    security = ClassSecurityInfo()
 
+    security.declarePrivate('getSource')
     def getSource(self, instance):
         # XXX: setter and getters for the adapter name
         source = u'local'
         return component.getMultiAdapter(
             (self, instance), ISource, name=source)
+
+registerField(DataTableField, 
+    title='Data Table', description='Complex data table with flexible sources')
 
 
 def availableTableStorages(context):
