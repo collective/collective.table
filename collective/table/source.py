b5f1c9d171719f3477f20e9b95d4dcafd23f6afb
Martijn Pieters <mj@jarn.com>
2011-02-17 15:18:12 +0000

    Commit the base storage class as well.
    git-svn-id: http://svn.plone.org/svn/collective/collective.table/trunk@232276 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/table/source.py
@@ -0,0 +1,27 @@
+# Table data sources are masquerading as Archetypes Storages.
+from zope import component
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.interfaces import IBaseObject
+from Products.Archetypes.Storage import Storage
+from Products.Archetypes.utils import setSecurity
+
+from .interfaces import ITableField
+
+
+class BaseSource(Storage):
+    component.adapts(ITableField, IBaseObject)
+    security = ClassSecurityInfo()
+
+    title = None
+    description = None
+    configurationView = None
+
+    def __init__(self, field, instance):
+        self.field = field
+        self.instance = instance
+
+    security.declarePrivate('listColumns')
+    def listColumns(self):
+        raise NotImplementedError()
+
+setSecurity(BaseSource)
