7a146e73fef00b1ed56127a7dd707e5e98dc3504
Martijn Pieters <mj@jarn.com>
2011-02-18 16:25:08 +0000

    Include TableTools to allow easy CSV, XLS export, printing.
    git-svn-id: http://svn.plone.org/svn/collective/collective.table/trunk@232347 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- collective/table/browser/resources/collective.table.js
+++ collective/table/browser/resources/collective.table.js
@@ -1,10 +1,14 @@
 if (typeof(collective) == 'undefined')
     collective = {};
 collective.table = (function($) {
+    TableToolsInit.sSwfPath = portal_url + 
+        "/++resource++jquery.datatables/extras/TableTools/" +
+        "media/swf/ZeroClipboard.swf";
     var Table = function(table, url, columns) {
         var self = this;
 
         self.table = table.dataTable({
+            sDom: 'T<"clear">lfrtip',
             aoColumns: columns,
             sAjaxSource: url
         })
@@ -12,5 +16,5 @@ collective.table = (function($) {
 
     return {
         Table: Table
-    }
+    };
 })(jQuery);
