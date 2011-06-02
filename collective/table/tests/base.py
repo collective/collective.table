50bf21b826cff02c292dad1d94f970aae7fc8807
Nejc Zupan <nejc.zupan@gmail.com>
2011-06-02 10:08:04 +0200

    created TestCases that can be later used by our tests

---
+++ collective/table/tests/base.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""Layers and TestCases for our tests."""
+
+from __future__ import with_statement
+
+import unittest2 as unittest
+
+from plone.testing import z2
+from plone.app.testing import applyProfile
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import PLONE_FIXTURE
+
+
+class TableLayer(PloneSandboxLayer):
+    """Layer for collective.table tests."""
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        import collective.table
+        self.loadZCML(package=collective.table)
+        z2.installProduct(app, 'collective.table')
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        applyProfile(portal, 'collective.table:default')
+
+    def tearDownZope(self, app):
+        """Tear down Zope."""
+        z2.uninstallProduct(app, 'collective.table')
+
+
+# FIXTURES
+TABLE_FIXTURE = TableLayer()
+
+# LAYERS
+INTEGRATION_TESTING = IntegrationTesting(
+    bases=(TABLE_FIXTURE, ),
+    name="collective.table:Integration")
+
+FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(TABLE_FIXTURE,),
+    name="collective.table:Functional")
+
+
+# TESTCASES
+class TableIntegrationTestCase(unittest.TestCase):
+    """We use this base class for all the tests in this package. If necessary,
+    we can put common utility or setup code in here. This applies to unit
+    test cases.
+    """
+    layer = INTEGRATION_TESTING
+
+
+class TableFunctionalTestCase(unittest.TestCase):
+    """We use this base class for all functional tests in this package -
+    tests that require a full-blown Plone instance for testing.
+    """
+    layer = FUNCTIONAL_TESTING
