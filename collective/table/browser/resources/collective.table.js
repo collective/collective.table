d7830a611c6d2dbe7e78003ca09235831568f753
Nejc Zupan <nejc.zupan@gmail.com>
2011-06-15 15:22:26 +0200

    make it work with the latest jquery.dataTables (v1.8)

--- collective/table/browser/resources/collective.table.js
+++ collective/table/browser/resources/collective.table.js
@@ -1,14 +1,15 @@
 if (typeof(collective) == 'undefined')
     collective = {};
 collective.table = (function($) {
-    TableToolsInit.sSwfPath = portal_url + 
-        "/++resource++jquery.datatables/extras/TableTools/" +
-        "media/swf/ZeroClipboard.swf";
+
     var Table = function(table, url, columns) {
         var self = this;
+        var swf_url = portal_url + "/++resource++jquery.datatables/extras/TableTools/" +
+                                   "media/swf/copy_cvs_xls.swf";
 
         self.table = table.dataTable({
             sDom: 'T<"clear">lfrtip',
+            oTableTools: { sSwfPath: swf_url },
             aoColumns: columns,
             sAjaxSource: url
         })
