2635678091c97c627a81739c6ecccefef1567f07
Nejc Zupan <nejc.zupan@gmail.com>
2011-06-29 13:41:04 +0300

    added jQuery.jEditable integrationx

--- collective/table/browser/resources/collective.table.js
+++ collective/table/browser/resources/collective.table.js
@@ -17,6 +17,21 @@ collective.table = (function($) {
             bServerSide: true,        // make an XHR request to the server for each draw of the information on the page
             aoColumns: columns,       // give DataTables specific instructions for each individual column -> don't read from DOM
             sAjaxSource: url + 'json_data',    // url from which DataTables should load the remote data
+            fnDrawCallback: function () {      // each time data is returned from the server, DataTables will build new DOM elements,
+                                               // so these need the jEditable event handlers applied to them
+                $('td', self.table.fnGetNodes()).editable(url + 'update_cell', {
+                    submitdata: function ( value, settings ) {                      // what to submit besides cell value
+                        var rowId = self.table.fnGetPosition(this)[0];
+                        var columnId = self.table.fnGetPosition(this)[2];
+                        var sColumnName = self.table.fnSettings().aoColumns[columnId].sName;
+                        return {
+                            row_id: rowId,                      // numerical id (int) of row
+                            column_name: sColumnName,           // string id (dict key) of column
+                        };
+                    },
+                    height: "14px"      // height of the editing text-field
+                });
+            }
         });
     };
 
@@ -24,10 +39,3 @@ collective.table = (function($) {
         Table: Table
     };
 })(jQuery);
-
-
-function fnClickAddRow() {
-    $('#table-table-datagrid').dataTable().fnAddData( [
-        "1",
-        "2"] );
-}
